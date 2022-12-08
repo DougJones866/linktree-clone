0f78867c9544b181bfc84ed994cb3d2eeb22d02c
kibroman <you@example.comkibromanx@gmail.com>
2022-12-07 22:21:50 -0800

    deploy

---
+++ js/chunk-2d21a3d2.c142456c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(n,c,t){"use strict";t.r(c);var e=t("7a23");const o={class:"home"};function s(n,c,t,s,b,d){const a=Object(e["v"])("links");return Object(e["o"])(),Object(e["d"])("div",o,[Object(e["f"])(a)])}var b={},d=t("6b0d"),a=t.n(d);const r=a()(b,[["render",s]]);c["default"]=r}}]);
+//# sourceMappingURL=chunk-2d21a3d2.c142456c.js.map
---
+++ js/chunk-2d22d746.027a80d6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23");const a={class:"about"},o=Object(e["e"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(e["o"])(),Object(e["d"])("div",a,s)}var d=c("6b0d"),b=c.n(d);const i={},r=b()(i,[["render",u]]);t["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.027a80d6.js.map
